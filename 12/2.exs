b2fbf6604127d3d4d85ea86641140f6f6d075735
Enrique Fernandez <enrique@bluelabs.eu>
2023-12-12 17:27:25 +0100

    fix: optimize day 12 part 2

--- 12/2.exs
+++ 12/2.exs
@@ -14,12 +14,8 @@ defmodule AOC23.D12 do
       {rec, sol}
     end)
     |> Enum.reduce(0, fn({rec, sol}, acc) ->
-    #|> pmap(fn({rec, sol}) ->
-      #IO.puts "XXX"
       acc + expand(rec, sol)
-      #expand(rec, sol)
     end)
-    #|> Enum.sum
   end
 
   def pmap(collection, f) do
@@ -28,38 +24,42 @@ defmodule AOC23.D12 do
     |> Enum.map(fn({:ok, res}) -> res end)
   end
 
-  def expand(xs, _sol, prev \\ nil, n \\ 0, acc \\ 0)
+  def expand(xs, _sol, prev \\ nil, n \\ 0)
 
-  def expand(<<>>,  [], _prev, _n, acc), do: acc + 1
-  def expand(<<>>, [n],   "#",  n, acc), do: acc + 1
+  def expand(<<>>,  [], _prev, _n), do: 1
+  def expand(<<>>, [n],   "#",  n), do: 1
 
-  def expand(       <<>>,      _,     _,  _, acc),
-    do: acc
-  def expand(<<"#",  _ :: binary>>,     [], _prev, _n, acc),
-    do: acc
-  def expand(<<"#",  _ :: binary>>, [n| _], _prev,  n, acc),
-    do: acc
-  def expand(        _xs, [s| _], _prev,  n, acc) when n > s,
-    do: acc
-  def expand(<<".",  _ :: binary>>, [s| _],   "#",  n, acc) when n < s,
-    do: acc
+  def expand(       <<>>,      _,     _,  _),
+    do: 0
+  def expand(<<"#",  _ :: binary>>,     [], _prev, _n),
+    do: 0
+  def expand(<<"#",  _ :: binary>>, [n| _], _prev,  n),
+    do: 0
+  def expand(        _xs, [s| _], _prev,  n) when n > s,
+    do: 0
+  def expand(<<".",  _ :: binary>>, [s| _],   "#",  n) when n < s,
+    do: 0
 
-  def expand(<<"?", xs :: binary>>, [s| _s] = sol, prev, n, acc) when n >= s,
-    do: expand("." <> xs, sol, prev, n, acc)
-  def expand(<<"?", xs :: binary>>, sol, prev, n, acc),
-    do: expand("." <> xs, sol, prev, n, expand("#" <> xs, sol, prev, n, acc))
-  def expand(<<".", xs :: binary>>, [_| sol], "#", _n, acc),
-    do: expand(xs, sol, ".", 0, acc)
-  def expand(<<".", xs :: binary>>, sol, _, _n, acc),
-    do: expand(xs, sol, ".", 0, acc)
-  def expand(<<"#", xs :: binary>>, sol, _prev, n, acc),
-    do: expand(xs, sol, "#", n + 1, acc)
+  def expand(<<"?", xs :: binary>>, [s| _s] = sol, prev, n) when n >= s,
+    do: mexpand("." <> xs, sol, prev, n)
+  def expand(<<"?", xs :: binary>>, sol, prev, n),
+    do: mexpand("." <> xs, sol, prev, n) + mexpand("#" <> xs, sol, prev, n)
+  def expand(<<".", xs :: binary>>, [_| sol], "#", _n),
+    do: mexpand(xs, sol, ".", 0)
+  def expand(<<".", xs :: binary>>, sol, _, _n),
+    do: mexpand(xs, sol, ".", 0)
+  def expand(<<"#", xs :: binary>>, sol, _prev, n),
+    do: mexpand(xs, sol, "#", n + 1)
 
-  # def memoized(key, fun) do
-  #   with nil <- Process.get(key) do
-  #     fun.() |> tap(&Process.put(key, &1))
-  #   end
-  # end
+  def mexpand(xs, sol, prev, n) do
+    memoized({xs, sol, n}, fn() -> expand(xs, sol, prev, n) end)
+  end
+
+  def memoized(key, fun) do
+    with nil <- Process.get(key) do
+      fun.() |> tap(&(Process.put(key, &1)))
+    end
+  end
 end
 
 

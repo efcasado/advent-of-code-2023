e4c876c26310a374c3922fa84ea3d30a1d24ff70
Enrique Fernandez <enrique@bluelabs.eu>
2023-12-15 00:32:11 +0100

    feat: solve day 14 part 2

--- 14/2.exs
+++ 14/2.exs
@@ -1,7 +1,13 @@
 defmodule AOC23.D14 do
   def run(input) do
-    parse(input) # west
-    |> cycle(1_000)
+    grid = parse(input) # west
+
+    {clength, cstart} = find_cycle(grid, &cycle/1)
+
+    iters = cstart + rem(1_000_000_000 - cstart, clength)
+
+    grid
+    |> cycle(iters)
     #|> IO.inspect
     |> load
   end
@@ -19,6 +25,26 @@ defmodule AOC23.D14 do
     |> east
 
     cycle(m, n - 1)
+  end
+
+  def find_cycle(x0, f) do
+    lambda = find_lambda(f, x0, f.(x0), 1, 1)
+    hare = Enum.reduce(1..lambda, x0, fn _,hare -> f.(hare) end)
+    mu = find_mu(f, x0, hare, 0)
+    {lambda, mu}
+  end
+
+  # Find lambda, the cycle length
+  defp find_lambda(_, tortoise, hare, _, lambda) when tortoise==hare, do: lambda
+  defp find_lambda(f, tortoise, hare, power, lambda) do
+    if power == lambda, do: find_lambda(f, hare, f.(hare), power*2, 1),
+                      else: find_lambda(f, tortoise, f.(hare), power, lambda+1)
+  end
+
+  # Find mu, the zero-based index of the start of the cycle
+  defp find_mu(_, tortoise, hare, mu) when tortoise==hare, do: mu
+  defp find_mu(f, tortoise, hare, mu) do
+    find_mu(f, f.(tortoise), f.(hare), mu+1)
   end
 
   def load(m) do

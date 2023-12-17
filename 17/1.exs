e324572118cd8902f2bca265f8f23887cd94feec
Enrique Fernández <enrique@bluelabs.eu>
2023-12-17 13:43:36 +0100

    chore: prepare day 17

--- 17/1.exs
+++ 17/1.exs
@@ -4,18 +4,63 @@ defmodule AOC23.D17 do
   @left  { 0, -1}
   @right { 0,  1}
 
-  def run(input, s, d) do
+  def run(input) do
     {size, g} =
       parse(input)
-      |> IO.inspect(limit: :infinity)
+      #|> IO.inspect(limit: :infinity)
     
     s = source(size)
     t = target(size)
 
-    IO.puts "from=#{inspect s} to=#{inspect t}"
     distance(g, s, t)
   end
 
+  def distance(g, from, to) do
+    {dist, _prev} = distances(g, from, to)
+    #|> IO.inspect(limit: :infinity)
+
+    dist[to]
+  end
+
+  def distances(g, from, to) do
+    distances(g, to, [{from, 0}], %{from => 0}, %{})
+  end
+
+  def distances(_g, _t, [], dist, prev) do
+    {dist, prev}
+  end
+  def distances(g, t, q0, dist, prev) do
+    {u, q} = min(q0)
+    case t == u do
+      true ->
+        {dist, prev}
+      false ->
+        ns = neighbors(g, u, 0, nil)
+        {dist1, prev1, q1} =
+          Enum.reduce(ns, {dist, prev, q},
+            fn({v, w}, {d, p, q}) ->
+              alt = d[u] + w
+              case alt < d[v] do
+                true  -> {Map.put(d, v, alt), Map.put(p, v, u), [{v, alt}| q]} # TODO: Use proper priority queue
+                false -> {d, p, q}
+              end
+            end)
+        distances(g, t, q1, dist1, prev1)
+    end
+  end
+
+  def min(q) do
+    # TODO: Use proper priority queue
+    {u, w} = Enum.min(q)
+    {u, q -- [{u, w}]}
+  end
+  
+  def neighbors(g, u, _steps, _direction) do
+    # TODO: Take steps and direction into account
+    {_w, ns} = Map.get(g, u)
+    Enum.map(ns, fn(n) -> {w, _ns} = Map.get(g, n); {n, w} end)
+  end
+  
   def source({{i, _}, {j, _}}), do: {i, j}
   def target({{_, i}, {_, j}}), do: {i, j}
   
@@ -47,18 +92,10 @@ defmodule AOC23.D17 do
   def vadd({i1, j1}, {i2, j2}), do: {i1 + i2, j1 + j2}
 
   def validn?(m, n), do: Map.get(m, n, nil) != nil
-  
-  def distance(_g, _from, _to) do
-    0
-  end
-
-  def neighbors(_u, _steps, _direction) do
-    []
-  end
 end
 
 
 input     = IO.stream(:stdio, :line)
-result    = AOC23.D17.run(input, {1, 0}, {0, 1})
+result    = AOC23.D17.run(input)
 
 IO.puts(result)

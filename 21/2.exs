ffee92dd5c6fd42a482f540fbc6641e1d163ec39
Enrique Fernández <enrique@bluelabs.eu>
2023-12-21 19:45:33 +0100

    chore: prepare day 21 part 2

--- 21/2.exs
+++ 21/2.exs
@@ -9,13 +9,43 @@ defmodule AOC23.D21 do
     {size, start, m} = parse(input)
     #|> IO.inspect(limit: :infinity)
 
-    bfs(size, m, [{start, 0}], 100, MapSet.new())
+    #bfs(size, m, [{start, 0}], 100, MapSet.new())
+    xxx(size, m, start, 1, 50, 0)
     #|> IO.inspect
     |> Enum.count
   end
 
+  def xxx(steps, prev, limit) when steps > limit do
+    :ok
+  end
+  def xxx(size, m, start, current, limit, prev) do
+    res = bfs(size, m, [{start, 0}], current, MapSet.new())
+    IO.puts "diff = #{res - prev}"
+    xxx(size, m, start, current + 1, limit, res - prev)
+  end
+  
+  def find_cycle(x0, f) do
+    lambda = find_lambda(f, x0, f.(x0), 1, 1)
+    hare = Enum.reduce(1..lambda, x0, fn _,hare -> f.(hare) end)
+    mu = find_mu(f, x0, hare, 0)
+    {lambda, mu}
+  end
+
+  # Find lambda, the cycle length
+  defp find_lambda(_, tortoise, hare, _, lambda) when tortoise==hare, do: lambda
+  defp find_lambda(f, tortoise, hare, power, lambda) do
+    if power == lambda, do: find_lambda(f, hare, f.(hare), power*2, 1),
+                      else: find_lambda(f, tortoise, f.(hare), power, lambda+1)
+  end
+
+  # Find mu, the zero-based index of the start of the cycle
+  defp find_mu(_, tortoise, hare, mu) when tortoise==hare, do: mu
+  defp find_mu(f, tortoise, hare, mu) do
+    find_mu(f, f.(tortoise), f.(hare), mu+1)
+  end
+  
   def bfs(_size, _m, [{_p, s}| _], limit, seen) when s > limit do
-    Enum.filter(seen, fn({_p, s}) -> s == limit end)
+    Enum.count(Enum.filter(seen, fn({_p, s}) -> s == limit end))
   end
   def bfs(size, m, [{p, s} = n| ns0], limit, seen) do
     case MapSet.member?(seen, n) do

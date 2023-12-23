263c145d83b379fb796e37cafbaf2a92072ff252
Enrique Fernandez <enrique@bluelabs.eu>
2023-12-23 22:08:42 +0100

    chore: prepare day 22 part 1

--- 22/1.exs
+++ 22/1.exs
@@ -1,25 +1,67 @@
 defmodule AOC23.D22 do
   def run(input) do
-    bricks = parse(input)
+    _bricks = parse(input)
     #|> IO.inspect(limit: :infinity)
     |> Enum.sort_by(fn({_, {_x, _y, {z1, z2}}}) -> {z1, z2} end)
+    #|> IO.inspect
+    |> fall
     |> IO.inspect
-    #|> fall
-
-    0
-  end
-
-  def fall([_]) do
-    :ok
-  end
-  def fall([a, b| xs]) do
-    atop?(b, a)
+    |> removable
     |> IO.inspect
-    fall([b| xs])
+    |> Enum.count
   end
 
-  def atop?({_id1, {x1, y1, _z1}}, {_id2, {x2, y2, _z2}}) do
-    overlap?(x1, x2) and overlap?(y1, y2)
+  def removable(bricks) do
+    _removable(bricks, bricks, [])
+  end
+
+  def _removable([_], _all, acc) do
+    Enum.uniq(acc)
+  end
+  def _removable([b| bs1], all, acc) do
+    case Enum.filter(all, fn(b2) -> atop2?(b, b2) end) do
+      [_] -> _removable(bs1, all, acc)
+      []  -> _removable(bs1, all, [b| acc])
+      bs2 -> _removable(bs1, all, bs2 ++ acc)
+    end
+  end
+
+  def atop2?({id, _}, {id, _}) do
+    false
+  end
+  def atop2?({_id1, {x1, y1, {z11, z12}}}, {_id2, {x2, y2, {z21, z22}}}) do
+    (z11 == z21 + 1 or z12 == z22 + 1) and overlap?(x1, x2) and overlap?(y1, y2)
+  end
+
+  def fall(bs, acc \\ [])
+  def fall([], acc) do
+    Enum.sort_by(acc, fn({_, {_x, _y, {z1, z2}}}) -> {z1, z2} end)
+  end
+  def fall([{id, {x, y, z}} = b1| bs1], acc) do
+    case Enum.filter(acc, fn(b2) -> atop?(b1, b2) end) do
+      []  -> fall(bs1, [{id, {x, y, {1, 1}}}| acc])
+      bs2 ->
+        {z1, z2} = adjust_z(bs2, z)
+        fall(bs1, [{id, {x, y, {z1, z2}}}| acc])
+    end
+  end
+
+  def adjust_z(bs, {z1, z2}) do
+    diff = z2 - z1
+
+    z1 = bs
+    |> Enum.flat_map(fn({_id, {_x, _y, {z1, z2}}}) -> [z1, z2] end)
+    |> Enum.max
+    |> Kernel.+(1)
+
+    {z1, z1 + diff}
+  end
+
+  def atop?({id, _}, {id, _}) do
+    false
+  end
+  def atop?({_id1, {x1, y1, {z11, z12}}}, {_id2, {x2, y2, {z21, z22}}}) do
+    (z11 > z21 or z12 > z22) and overlap?(x1, x2) and overlap?(y1, y2)
   end
 
   def overlap?({a1, a2}, {b1, b2}), do: not Range.disjoint?(a1..a2, b1..b2)
